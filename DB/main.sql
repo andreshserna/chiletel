52d515beebdac0e5cd3dc9fd0a708884d79209d3
andreshserna <mauricioserna123@gmail.com>
2023-10-21 12:41:02 -0500

    fix main initializer of DB

--- DB/main.sql
+++ DB/main.sql
@@ -1,70 +1,51 @@
-CREATE PLUGGABLE DATABASE TALLER_PRUEBA_TECHCAMP
-ADMIN USER tpt_admin IDENTIFIED BY &admin_pass
-DEFAULT TABLESPACE TS_MANAGER
-DATAFILE 'C:/taller_techcamp_datafiles/TALLER_PRUEBA_TECHCAMP.dbf' SIZE 500M AUTOEXTEND ON
-FILE_NAME_CONVERT = ('C:\APP\CHIEFPC\PRODUCT\21C\ORADATA\XE\PDBSEED\', 'C:/taller_techcamp_datafiles/');
-
-
+-- Create Pluggable Database
+CREATE PLUGGABLE DATABASE CHILETEL_PRUEBA
+ADMIN USER chile_admin IDENTIFIED BY &admin_pass
+DATAFILE 'C:/chiletel_datafiles/CHILETEL_PRUEBA.dbf' SIZE 500M AUTOEXTEND ON
+FILE_NAME_CONVERT = ('C:\APP\CHIEFPC\PRODUCT\21C\ORADATA\XE\PDBSEED\', 'C:/chiletel_datafiles/');
 
 -- Set permissions
-@./sys/permissions/TPT_ROL_USER_CONEXION.sql
-@./sys/permissions/TPT_ROL_USER_DBA.sql
--- @./sys/permissions/TPT_ROL_USER_PROCEDATOS.sql
-
--- Crete tablespaces
--- @./sys/tablespaces/TPT_TABLESPACE.sql
-@./sys/tablespaces/TPT_TS_CONNECTION.sql
-@./sys/tablespaces/TPT_TS_MANAGER.sql
-
--- Create users
-@./sys/users/TPT_USER_DBA.sql
--- @./sys/users/TPT_USER_FINAL_1.sql
--- @./sys/users/TPT_USER_FINAL_2.sql
-
-
+@./sys/permissions/CHILE_ROL_USER_CONEXION.sql
+@./sys/permissions/CHILE_ROL_USER_DBA.sql
+ 
+@./sys/users/CHILE_USER_DBA.sql
 
 -- Create tables
-@./tables/TPT_ACCOUNT_TYPE.sql
-@./tables/TPT_DOCUMENT_TYPE.sql
-@./tables/TPT_TRANSACTION_TYPE.sql
-@./tables/TPT_BANK_BRANCH.sql
 @./tables/TPT_CUSTOMER.sql
-@./tables/TPT_ACCOUNT.sql
-@./tables/TPT_TRANSACTION.sql
+@./tables/TPT_DAMAGE.sql
+@./tables/TPT_TECHNICIAN.sql
+@./tables/TPT_CREW.sql
+@./tables/TPT_ATTENTION_ORDER.sql
+@./tables/TPT_TECHNICIAN_SPECIALTY.sql
 
--- Create sequences
-@./sequences/SEQ_ACCOUNT_TYPE.sql
-@./sequences/SEQ_DOCUMENT_TYPE.sql
-@./sequences/SEQ_TRANSACTION_TYPE.sql
-@./sequences/SEQ_BANK_BRANCH.sql
+-- Create sequences CEHCJKCNECEJHNCUAHSECUEJSFHEHUF
 @./sequences/SEQ_CUSTOMER.sql
-@./sequences/SEQ_ACCOUNT.sql
-@./sequences/SEQ_TRANSACTION.sql
+@./sequences/SEQ_DAMAGE.sq
+@./sequences/SEQ_TECHNICIAN.sql
+@./sequences/SEQ_CREW.sql
+@./sequences/SEQ_ATTENTION_ORDER.sql
+@./sequences/SEQ_TECHNICIAN_SPECIALTY.sql
 
 -- Create PKs
-@./indexes/PK_ACCOUNT_TYPE.sql
-@./indexes/PK_ACCOUNT.sql
-@./indexes/PK_BANK_BRANCH.sql
 @./indexes/PK_CUSTOMER.sql
-@./indexes/PK_DOCUMENT_TYPE.sql
-@./indexes/PK_TRANSACTION_TYPE.sql
-@./indexes/PK_TRANSACTION.sql
+@./indexes/PK_DAMAGE.sql
+@./indexes/PK_TECHNICIAN.sql
+@./indexes/PK_CREW.sql
+@./indexes/PK_ATTENTION_ORDER.sql
+@./indexes/PK_TECHNICIAN_SPECIALTY.sql
 
--- Add indexes
-@./indexes/IDX_ACCOUNT_TYPE.sql
-@./indexes/IDX_ACCOUNT.sql
-@./indexes/IDX_BANK_BRANCH.sql
+-- Add indexes fadfawsfased fdesfdsfsdfsd
 @./indexes/IDX_CUSTOMER.sql
-@./indexes/IDX_DOCUMENT_TYPE.sql
-@./indexes/IDX_TRANSACTION_TYPE.sql
-@./indexes/IDX_TRANSACTION.sql
+@./indexes/IDX_DAMAGE.sql
+@./indexes/IDX_TECHNICIAN.sql
+@./indexes/IDX_CREW.sql
+@./indexes/IDX_ATTENTION_ORDER.sql
+@./indexes/IDX_TECHNICIAN_SPECIALTY.sql
 
 -- Set references
-@./references/FK_ACCOUNT_ACCOUNT_TYPE.sql
-@./references/FK_ACCOUNT_BANK_BRANCH.sql
-@./references/FK_ACCOUNT_CUSTOMER.sql
-@./references/FK_CUSTOMER_DOCUMENT_TYPE.sql
-@./references/FK_TRANSACTION_ACCOUNT_01.sql
-@./references/FK_TRANSACTION_ACCOUNT_02.sql
-@./references/FK_TRANSACTION_BANK_BRANCH.sql
-@./references/FK_TRANSACTION_TRANSACTION_TYPE.sql
+@./references/FK_ATTENTION_ORDER_CUSTOMER.sql
+@./references/FK_ATTENTION_ORDER_DAMAGE.sql
+@./references/FK_ATTENTION_ORDER_TECHNICIAN.sql
+@./references/FK_TECHNICIAN_CREW.sql
+@./references/FK_TECHNICIAN_SPECIALTY_TECHNICIAN.sql
+@./references/FK_TECHNICIAN_SPECIALTY_DAMAGE.sql
