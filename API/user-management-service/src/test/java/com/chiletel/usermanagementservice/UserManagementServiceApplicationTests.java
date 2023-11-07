228b01415f725cda8bdf6b9fd054915b1f595516
Andrés Hernández Serna <mauricioserna123@gmail.com>
2023-11-07 16:26:34 -0500

    api

---
+++ API/user-management-service/src/test/java/com/chiletel/usermanagementservice/UserManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chiletel.usermanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
