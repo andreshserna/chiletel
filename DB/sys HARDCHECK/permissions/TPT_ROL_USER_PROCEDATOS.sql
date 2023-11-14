eb6e6dcf38314c5786518f06467871c3e3c5a1ad
Andrés Hernández Serna <mauricioserna123@gmail.com>
2023-11-14 06:43:37 -0500

    fix

--- DB/sys HARDCHECK/permissions/TPT_ROL_USER_PROCEDATOS.sql
+++
@@ -1,9 +0,0 @@
-/*******************************************************************************
-Description: Table that stores information about bank's transactions
-Author: Andres Mauricio Hernandez Serna
-Date 22-09-23
-Management Id: XD01
-@copyright: Seguros Bolívar
-*******************************************************************************/
-
-/
